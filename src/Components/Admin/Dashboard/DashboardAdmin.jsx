bbb6777ff4d1c37ef3769b5b2fcce8c4aeaeb480
dikamantab <dikamantab97@gmail.com>
2025-01-25 08:38:15 +0700

    main

--- src/Components/Admin/Dashboard/DashboardAdmin.jsx
+++ src/Components/Admin/Dashboard/DashboardAdmin.jsx
@@ -30,42 +30,61 @@ const DashboardAdmin = () => {
           useEffect(() => {
                     const fetchData = async () => {
                               try {
-                                        // Simulasi data berdasarkan tahun
+                                        // Simulasi data berdasarkan tahun yang dipilih
                                         const dataFromApi = await new Promise((resolve) => {
                                                   setTimeout(() => {
-                                                            resolve({
-                                                                      labels: [
-                                                                                "Jan",
-                                                                                "Feb",
-                                                                                "Mar",
-                                                                                "Apr",
-                                                                                "Mei",
-                                                                                "Jun",
-                                                                                "Jul",
-                                                                                "Agu",
-                                                                                "Sep",
-                                                                                "Okt",
-                                                                                "Nov",
-                                                                                "Des",
-                                                                      ],
-                                                                      datasets: [
-                                                                                {
-                                                                                          label: "Total Pengguna",
-                                                                                          data: [250, 280, 290, 310, 240, 200, 400, 300, 280, 250, 290, 200],
-                                                                                          backgroundColor: "skyblue",
-                                                                                },
-                                                                                {
-                                                                                          label: "Pembimbing",
-                                                                                          data: [200, 250, 270, 290, 220, 180, 350, 280, 250, 220, 270, 180],
-                                                                                          backgroundColor: "royalblue",
-                                                                                },
-                                                                                {
-                                                                                          label: "Mahasiswa",
-                                                                                          data: [300, 320, 350, 330, 280, 220, 450, 350, 300, 280, 320, 250],
-                                                                                          backgroundColor: "mediumblue",
-                                                                                },
-                                                                      ],
-                                                            });
+                                                            const yearlyData = {
+                                                                      2020: {
+                                                                                labels: ["Jan", "Feb", "Mar", "Apr", "Mei", "Jun", "Jul", "Agu", "Sep", "Okt", "Nov", "Des"],
+                                                                                datasets: [
+                                                                                          { label: "Total Pengguna", data: [100, 150, 200, 250, 300, 350, 400, 450, 500, 550, 600, 650], backgroundColor: "skyblue" },
+                                                                                          { label: "Pembimbing", data: [80, 120, 160, 200, 240, 280, 320, 360, 400, 440, 480, 520], backgroundColor: "royalblue" },
+                                                                                          { label: "Mahasiswa", data: [20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130], backgroundColor: "mediumblue" },
+                                                                                ],
+                                                                      },
+                                                                      2021: {
+                                                                                labels: ["Jan", "Feb", "Mar", "Apr", "Mei", "Jun", "Jul", "Agu", "Sep", "Okt", "Nov", "Des"],
+                                                                                datasets: [
+                                                                                          { label: "Total Pengguna", data: [150, 200, 250, 300, 350, 400, 450, 500, 550, 600, 650, 700], backgroundColor: "skyblue" },
+                                                                                          { label: "Pembimbing", data: [120, 160, 200, 240, 280, 320, 360, 400, 440, 480, 520, 560], backgroundColor: "royalblue" },
+                                                                                          { label: "Mahasiswa", data: [30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140], backgroundColor: "mediumblue" },
+                                                                                ],
+                                                                      },
+                                                                      2022: {
+                                                                                labels: ["Jan", "Feb", "Mar", "Apr", "Mei", "Jun", "Jul", "Agu", "Sep", "Okt", "Nov", "Des"],
+                                                                                datasets: [
+                                                                                          { label: "Total Pengguna", data: [150, 200, 250, 300, 350, 200, 450, 500, 550, 600, 650, 700], backgroundColor: "skyblue" },
+                                                                                          { label: "Pembimbing", data: [120, 160, 200, 240, 280, 320, 360, 500, 440, 480, 520, 560], backgroundColor: "royalblue" },
+                                                                                          { label: "Mahasiswa", data: [30, 40, 50, 60, 70, 80, 90, 100, 110, 410, 130, 140], backgroundColor: "mediumblue" },
+                                                                                ],
+                                                                      },
+                                                                      2023: {
+                                                                                labels: ["Jan", "Feb", "Mar", "Apr", "Mei", "Jun", "Jul", "Agu", "Sep", "Okt", "Nov", "Des"],
+                                                                                datasets: [
+                                                                                          { label: "Total Pengguna", data: [150, 200, 550, 300, 350, 200, 450, 500, 550, 600, 650, 700], backgroundColor: "skyblue" },
+                                                                                          { label: "Pembimbing", data: [120, 160, 200, 290, 280, 320, 360, 500, 440, 480, 520, 560], backgroundColor: "royalblue" },
+                                                                                          { label: "Mahasiswa", data: [30, 40, 50, 60, 270, 80, 90, 100, 110, 410, 130, 140], backgroundColor: "mediumblue" },
+                                                                                ],
+                                                                      },
+                                                                      2024: {
+                                                                                labels: ["Jan", "Feb", "Mar", "Apr", "Mei", "Jun", "Jul", "Agu", "Sep", "Okt", "Nov", "Des"],
+                                                                                datasets: [
+                                                                                          { label: "Total Pengguna", data: [250, 280, 290, 310, 240, 200, 400, 300, 280, 250, 290, 200], backgroundColor: "skyblue" },
+                                                                                          { label: "Pembimbing", data: [200, 250, 270, 290, 220, 180, 350, 280, 250, 220, 270, 180], backgroundColor: "royalblue" },
+                                                                                          { label: "Mahasiswa", data: [300, 320, 350, 330, 280, 220, 450, 350, 300, 280, 320, 250], backgroundColor: "mediumblue" },
+                                                                                ],
+                                                                      },
+                                                                      2025: {
+                                                                                labels: ["Jan", "Feb", "Mar", "Apr", "Mei", "Jun", "Jul", "Agu", "Sep", "Okt", "Nov", "Des"],
+                                                                                datasets: [
+                                                                                          { label: "Total Pengguna", data: [300, 350, 400, 450, 500, 550, 600, 650, 700, 750, 800, 850], backgroundColor: "skyblue" },
+                                                                                          { label: "Pembimbing", data: [250, 300, 350, 400, 450, 500, 550, 600, 650, 700, 750, 800], backgroundColor: "royalblue" },
+                                                                                          { label: "Mahasiswa", data: [50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150, 160], backgroundColor: "mediumblue" },
+                                                                                ],
+                                                                      },
+                                                            };
+
+                                                            resolve(yearlyData[selectedYear]);
                                                   }, 1000);
                                         });
 
@@ -80,9 +99,9 @@ const DashboardAdmin = () => {
                                         const userDataFromApi = await new Promise((resolve) => {
                                                   setTimeout(() => {
                                                             resolve({
-                                                                      totalPengguna: 1440,
-                                                                      pembimbing: 1440,
-                                                                      mahasiswa: 1440,
+                                                                      totalPengguna: 1000 + selectedYear * 10, // Simulasi data total pengguna
+                                                                      pembimbing: 500 + selectedYear * 5, // Simulasi data pembimbing
+                                                                      mahasiswa: 500 + selectedYear * 5, // Simulasi data mahasiswa
                                                             });
                                                   }, 500);
                                         });
@@ -94,7 +113,7 @@ const DashboardAdmin = () => {
 
                     fetchData();
                     fetchUserData();
-          }, [selectedYear]); // Tambahkan selectedYear ke dependency array
+          }, [selectedYear]); // Perbarui data ketika selectedYear berubah
 
           const options = {
                     responsive: true,
@@ -108,12 +127,8 @@ const DashboardAdmin = () => {
                               },
                     },
                     scales: {
-                              x: {
-                                        stacked: true,
-                              },
-                              y: {
-                                        stacked: true,
-                              },
+                              x: { stacked: true },
+                              y: { stacked: true },
                     },
           };
 
@@ -131,9 +146,8 @@ const DashboardAdmin = () => {
 
           return (
                     <div className="dashboard-container">
-                              
                               <div className="card-container">
-                                        <div className="card card-purple">
+                                        <div className="card">
                                                   <FaUsers className="card-icon" />
                                                   <h2>Total Pengguna</h2>
                                                   <p>{userData.totalPengguna}</p>
@@ -167,7 +181,6 @@ const DashboardAdmin = () => {
                                                   ))}
                                         </select>
                               </div>
-
                               <div className="chart-container">
                                         <Bar options={options} data={chartData} />
                               </div>
